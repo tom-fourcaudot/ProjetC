84bb3de25f106422832bde8045482b96de96b4a2
Matis <matis.bouquin@etu.u-bordeaux.fr>
2023-10-16 11:56:20 +0200

    init rope

---
+++ cmake-build-debug/CMakeFiles/projetC.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/matisbouquin/Documents/Travail/IUT/S5/ProjetC/ProjetC/Rope/Node.c" "CMakeFiles/ProjetC.dir/Rope/Node.c.o" "gcc" "CMakeFiles/ProjetC.dir/Rope/Node.c.o.d"
+  "/Users/matisbouquin/Documents/Travail/IUT/S5/ProjetC/ProjetC/Rope/Rope.c" "CMakeFiles/ProjetC.dir/Rope/Rope.c.o" "gcc" "CMakeFiles/ProjetC.dir/Rope/Rope.c.o.d"
+  "/Users/matisbouquin/Documents/Travail/IUT/S5/ProjetC/ProjetC/main.c" "CMakeFiles/ProjetC.dir/main.c.o" "gcc" "CMakeFiles/ProjetC.dir/main.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/projetC.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjetC.dir/Rope/Node.c.o"
+  "CMakeFiles/ProjetC.dir/Rope/Node.c.o.d"
+  "CMakeFiles/ProjetC.dir/Rope/Rope.c.o"
+  "CMakeFiles/ProjetC.dir/Rope/Rope.c.o.d"
+  "CMakeFiles/ProjetC.dir/main.c.o"
+  "CMakeFiles/ProjetC.dir/main.c.o.d"
+  "ProjetC"
+  "ProjetC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ProjetC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
