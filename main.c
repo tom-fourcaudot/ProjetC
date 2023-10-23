1c1dc85defebeafb58f9e2c06afc88402e1f2039
tfourcaudot <tom.fourcaudot@gmail.com>
2023-10-23 12:12:57 +0200

    free all C string during init

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "Rope/Node.h"
-#include "Rope/Rope.h"
 
 int main() {
     unsigned int* sub_size = malloc(sizeof (unsigned int));
-    Rope rope = init_rope("abcdefghijklmnopqrstuvwxyz", 3);
+    *sub_size = 3;
+    char* string = malloc(26 * sizeof (char));
+    string = "abcdefghijklmnopqrstuvwxyz";
+    Node* node = init_node(string, sub_size);
+    printf("%d", node->leftNeighbour->label);
+    printf("%s", node->leftNeighbour->substring);
+    free(sub_size);
     return 0;
 }
-
