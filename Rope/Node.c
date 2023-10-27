a520fced5c78152bbb10341ef256ee802584feb5
tfourcaudot <tom.fourcaudot@gmail.com>
2023-10-27 11:56:31 +0200

    Fix label and string during node creation

--- Rope/Node.c
+++ Rope/Node.c
@@ -1,12 +1,10 @@
 //
 // Created by toum on 10/16/23.
 //
-#include <math.h>
 #include "Node.h"
 #include "String.h"
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
 
 Node * init_node(String* string, const unsigned int* substring_size) {
@@ -15,13 +13,15 @@ Node * init_node(String* string, const unsigned int* substring_size) {
         node->label = string->size;
         node->rightNeighbour = NULL;
         node->leftNeighbour = NULL;
-        char* substring = malloc(string->size * sizeof (char));
-        memcpy(substring, string->first_char, string->size);
-        node->substring = init_string(substring);
+        char* leafString = malloc((string->size + 1) * sizeof (char));
+        memcpy(leafString, string->first_char, string->size);
+        *(leafString + string->size) = '\0';
+        node->substring = init_string(leafString);
+        free(string);
     } else {
         node->rightNeighbour = init_node(cut_string(string), substring_size);
-        node->leftNeighbour = init_node(string, substring_size);
         node->label = string->size;
+        node->leftNeighbour = init_node(string, substring_size);
         node->substring = NULL;
     }
     return node;
